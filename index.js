4f7b398cd3ed1d423775bd4b16e58afcc7fff63e
Jeni Dang <jenipdang@yahoo.com>
2021-11-06 20:57:13 -0700

    commit tree 1dbd2d44e37aa3ce779a26a93f6ef0fdfd993b29

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+
+
+class Circle{
+    constructor(radius){
+        this.radius = radius
+    }
+
+    get diameter(){ //corrrect
+        return `${this.radius}` * 2
+    }
+
+    set diameter(diameter){
+        this.radius = diameter / 2
+    }
+    get circumference(){ //correct 
+        return 2 * Math.PI * `${this.radius}`
+    }
+
+    set circumference(circumference){
+        this.radius = circumference / (2 * Math.PI)
+    }
+    get area(){ //correct
+        return Math.PI * `${this.radius}` * `${this.radius}`
+    }
+
+    set area(area) {
+        this.radius = Math.sqrt (area/ Math.PI)
+    }
+    
+}
